5dbf383f075115ba503ef81beb03b7a4a859b04d
impedrohenri <impedrohenri@gmail.com>
2026-05-07 15:29:14 -0300

    feat: atualiza retorno ao exceder limite de requisições

--- app/middlewares/rate_limit.py
+++ app/middlewares/rate_limit.py
@@ -1,13 +1,17 @@
-from fastapi import Request, HTTPException
+from fastapi import Request
+from fastapi.responses import JSONResponse
 from starlette.middleware.base import BaseHTTPMiddleware
+
 from app.core.redis_client import redis_client
 
 MAX_REQUESTS = 20
 WINDOW_SECONDS = 10
 
+
 class RateLimitMiddleware(BaseHTTPMiddleware):
 
     async def dispatch(self, request: Request, call_next):
+
         ip = request.client.host
 
         key = f"rate_limit:{ip}"
@@ -15,9 +19,13 @@ class RateLimitMiddleware(BaseHTTPMiddleware):
         current = redis_client.get(key)
 
         if current and int(current) >= MAX_REQUESTS:
-            raise HTTPException(
+
+            return JSONResponse(
                 status_code=429,
-                detail="Too many requests"
+                content={
+                    "error": "rate_limit_exceeded",
+                    "message": "Too many requests. Try again later."
+                }
             )
 
         pipe = redis_client.pipeline()
